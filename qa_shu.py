3ee22341befddb277498291d7c7f7a68f7cc4de8
ZARUJU <0613akb48@gmail.com>
2025-05-31 20:16:00 +0900

    fix: 最新の会期を先に実行するように変更

--- qa_shu.py
+++ qa_shu.py
@@ -11,6 +11,13 @@ from src.utils import write_to_json, file_exists
 LATEST_SESSION = 217
 WAIT_SECOND = 5.0
 
+# 最新の会期は強制実行
+data = get_qa_shu_list_data(LATEST_SESSION)
+write_to_json(data.model_dump(), f"data/qa_shu/list/{LATEST_SESSION}.json")
+save_qa_shu_question_texts(LATEST_SESSION, WAIT_SECOND)
+save_qa_shu_answer_texts(LATEST_SESSION, WAIT_SECOND)
+
+
 # 最新会期までの質問主意書を取得する（未取得のみ）
 # 最新会期は取得済みでも取得する
 for session in range(1, LATEST_SESSION + 1):
@@ -25,9 +32,3 @@ for session in range(1, LATEST_SESSION + 1):
     # 質問主意書の取得と保存
     save_qa_shu_question_texts(session, WAIT_SECOND)
     save_qa_shu_answer_texts(session, WAIT_SECOND)
-
-# 最新の会期は強制実行
-data = get_qa_shu_list_data(LATEST_SESSION)
-write_to_json(data.model_dump(), f"data/qa_shu/list/{LATEST_SESSION}.json")
-save_qa_shu_question_texts(LATEST_SESSION, WAIT_SECOND)
-save_qa_shu_answer_texts(LATEST_SESSION, WAIT_SECOND)
